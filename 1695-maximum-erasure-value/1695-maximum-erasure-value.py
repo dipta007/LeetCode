e042f00fea429aaf51c522ef6f6683b46424a9d3
Shubhashis Roy Dipta <iamdipta@gmail.com>
2024-01-11 00:18:03 -0500

    Time: 941 ms (70.47%), Space: 30.3 MB (61.82%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.py
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.py
@@ -4,13 +4,16 @@ class Solution:
         count = defaultdict(int)
         res = 0
         l = 0
+        cum = 0
         for r in range(len(nums)):
             v = nums[r]
             count[v] += 1
+            cum += v
 
             while count[v] > 1:
                 count[nums[l]] -= 1
+                cum -= nums[l]
                 l += 1
             
-            res = max(res, sum(nums[l:r+1]))
+            res = max(res, cum)
         return res
