6224292fbecab2ae0d8285a2d73b883fd9867ea1
Shubhashis Roy Dipta <iamdipta@gmail.com>
2024-01-17 23:30:22 -0500

    Time: 1032 ms (10.61%), Space: 28.1 MB (35.92%) - LeetHub

---
+++ 2256-minimum-average-difference/2256-minimum-average-difference.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minimumAverageDifference(self, nums: List[int]) -> int:
+        mn = 1000000000
+        ind = 0
+
+        rgt = sum(nums)
+        lft = 0
+        for i in range(len(nums)):
+            v = nums[i]
+            lft += v
+            rgt -= v
+
+            lft_avg = lft // (i+1)
+            rgt_avg = rgt // ((len(nums) - i - 1) if i < len(nums)-1 else 1)
+            nw = abs(lft_avg - rgt_avg)
+
+            print(i, nw, mn)
+            if nw < mn:
+                mn = nw
+                ind = i
+
+        return ind
