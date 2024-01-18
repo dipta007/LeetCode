e7c09aa64ba6c19ff4611eddf86212998d56c163
Shubhashis Roy Dipta <iamdipta@gmail.com>
2024-01-17 22:04:19 -0500

    Time: 180 ms (83.87%), Space: 36.9 MB (16.89%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.py
+++ 0706-design-hashmap/0706-design-hashmap.py
@@ -1,15 +1,23 @@
 class MyHashMap:
 
     def __init__(self):
-        self.value = [-1 for _ in range(1000004)]
+        self.value = []
+
+    def resize(self, key):
+        if key >= len(self.value):
+            self.value = self.value + [-1] * (key + 1 - len(self.value))
 
     def put(self, key: int, value: int) -> None:
+        self.resize(key)
         self.value[key] = value
 
     def get(self, key: int) -> int:
+        if key >= len(self.value):
+            return -1
         return self.value[key]
 
     def remove(self, key: int) -> None:
+        if key >= len(self.value): return
         self.value[key] = -1
 
 
