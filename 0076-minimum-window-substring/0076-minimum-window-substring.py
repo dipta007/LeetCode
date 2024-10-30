e3d3a69b6a3eec07e197451c0fd9cab90c88f117
Shubhashis Roy Dipta <iamdipta@gmail.com>
2024-10-30 13:10:28 -0400

    Time: 118 ms (42.58%), Space: 17.1 MB (84.02%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,27 +1,30 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        from collections import Counter, defaultdict
-        mp = Counter(t)
-        res = ""
+        tcounter = {}
+        tcnt = 0
+        for c in t:
+            tcounter[c] = tcounter.get(c, 0) + 1
+        tcnt = len(tcounter)
+
+        scounter = {}
         l = 0
-        uni = 0
-        cnt = defaultdict(int)
+        scnt = 0
+        res = len(s) + 10
+        ret = ""
         for h in range(len(s)):
-            cnt[s[h]] += 1
-            while l < len(s) and cnt[s[l]] > mp[s[l]]:
-                cnt[s[l]] -= 1
+            c = s[h]
+            scounter[c] = scounter.get(c, 0) + 1
+            if scounter[c] == tcounter.get(c, 0):
+                scnt += 1
+            while scnt == tcnt:
+                # print(h, scnt, tcnt, l, scounter)
+                if h - l + 1 < res:
+                    ret = s[l:h+1]
+                    res = h - l + 1
+                lc = s[l]
+                scounter[lc] -= 1
                 l += 1
-
-            flg = 1
-            for k in mp.keys():
-                if mp[k] > cnt[k]:
-                    flg = 0
-                    break
-            # print(mp)
-            # print(cnt)
-            # print(l, h, flg)
-            if flg and (len(res) == 0 or (h-l+1) < len(res)):
-                res = s[l:h+1]
-        
-        return res
-
+                if scounter[lc] < tcounter.get(lc, 0):
+                    scnt -= 1
+            
+        return ret
