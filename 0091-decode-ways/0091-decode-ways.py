4db0d29e7bf4f40f19cd05447ba38021eb854d9c
Shubhashis Roy Dipta <iamdipta@gmail.com>
2024-10-31 12:51:44 -0400

    Time: 7 ms (10.29%), Space: 17 MB (7.23%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,13 +1,16 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
+        s = list(s)
+        s = [int(x) for x in s]
+
         @cache
         def call(i, curr):
-            if curr[0] == '0' or int(curr) > 26:
+            if curr == 0 or curr > 26:
                 return 0
             if i == len(s):
                 return 1
 
-            res = call(i+1, curr + s[i])
+            res = call(i+1, curr * 10 + s[i])
             res += call(i+1, s[i])
 
             return res
