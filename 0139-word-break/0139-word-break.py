b11aa325415a6e600f850fbcc4552914f4c85441
Shubhashis Roy Dipta <iamdipta@gmail.com>
2024-11-02 16:03:51 -0400

    Time: 7 ms (22.91%), Space: 18.1 MB (5.82%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,32 +1,38 @@
 class Node:
     def __init__(self):
-        self.child = {}
+        self.ltr = {}
         self.end = False
-    
+
 class Trie:
     def __init__(self):
         self.root = Node()
-
-    def insert(self, word):
+    
+    def insert(self, w):
         curr = self.root
-        for w in word:
-            if w not in curr.child:
-                curr.child[w] = Node()
-            curr = curr.child[w]
+        for c in w:
+            if c not in curr.ltr:
+                curr.ltr[c] = Node()
+            curr = curr.ltr[c]
         curr.end = True
     
-    @lru_cache
-    def dfs(self, i, w, curr):
-        if i >= len(w):
-            return curr.end
-        
-        res = False
-        if curr.end == True:
-            res = self.dfs(i, w, self.root)
-        
-        if w[i] in curr.child:
-            res = res or self.dfs(i+1, w, curr.child[w[i]])
-        return res
+    def search(self, w):
+        # print(len(w))
+        @cache
+        def call(i, curr):
+            if not curr:
+                return False
+            if i == len(w):
+                # print(i, len(w), curr.end)
+                return curr.end
+            
+            res = False
+            res = res or call(i+1, curr.ltr.get(w[i], None))
+            
+            if curr.end:
+                res = res or call(i, self.root)
+
+            return res
+        return call(0, self.root)
 
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
@@ -34,5 +40,4 @@ class Solution:
         for w in wordDict:
             tree.insert(w)
         
-        return tree.dfs(0, s, tree.root)
-        
+        return tree.search(s)
