c702faf7757eac980712b0085989b1c714fa28d0
Shubhashis Roy Dipta <iamdipta@gmail.com>
2024-11-03 11:58:17 -0500

    Time: 23 ms (53.22%), Space: 29.9 MB (59.04%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,11 +1,15 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        mp = {}
-        for i in range(len(nums)):
-            v = nums[i]
-            if v in mp:
-                nw = i - mp[v]
-                if nw <= k:
+        import bisect
+        mp_set = {}
+        for i, v in enumerate(nums):
+            if v not in mp_set:
+                mp_set[v] = i
+            else:
+                need = i - k
+                if mp_set[v] >= need:
                     return True
-            mp[v] = i
+            
+            mp_set[v] = i
+
         return False
