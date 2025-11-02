fc41f9aa1cc582d9e2c2b44c526c9b4234d5abf2
Shubhashis Roy Dipta <iamdipta@gmail.com>
2025-11-02 17:20:28 -0500

    Time: 246 ms (58.16%), Space: 20.9 MB (36.44%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -4,21 +4,20 @@ dy = [0, 0, -1, 1]
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         r, c = len(grid), len(grid[0])
-        vis = [[0] * c for _ in range(r)]
-        
-        def call(ux, uy):
-            vis[ux][uy] = 1
+        visited = [[0 for _ in range(c)] for __ in range(r)]
+        def dfs(ux, uy):
+            visited[ux][uy] = 1
+
             for dxx, dyy in zip(dx, dy):
                 vx = ux + dxx
                 vy = uy + dyy
-                if vx >= 0 and vy >= 0 and vx < r and vy < c and vis[vx][vy] == 0 and grid[vx][vy] == "1":
-                    call(vx, vy)
+                if vx >= 0 and vy >= 0 and vx < r and vy < c and visited[vx][vy] == 0 and grid[vx][vy] == '1':
+                    dfs(vx, vy)
         
-        res = 0
+        cnt = 0
         for i in range(r):
             for j in range(c):
-                if grid[i][j] == "1" and vis[i][j] == 0:
-                    res += 1
-                    call(i, j)
-        
-        return res
+                if visited[i][j] == 0 and grid[i][j] == '1':
+                    cnt += 1
+                    dfs(i, j)
+        return cnt
