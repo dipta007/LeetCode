16d367a33aaf392976ab0f1db754a014080f83f4
Shubhashis Roy Dipta <iamdipta@gmail.com>
2025-11-29 18:00:01 -0500

    Time: 15 ms (34.22%), Space: 24.8 MB (81.23%) - LeetHub

---
+++ 0359-logger-rate-limiter/0359-logger-rate-limiter.py
@@ -0,0 +1,17 @@
+class Logger:
+
+    def __init__(self):
+        self.print = {}
+        
+
+    def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
+        if message not in self.print or timestamp >= self.print[message]:
+            self.print[message] = timestamp + 10
+            return True
+        return False
+        
+
+
+# Your Logger object will be instantiated and called as such:
+# obj = Logger()
+# param_1 = obj.shouldPrintMessage(timestamp,message)
